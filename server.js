f2f0127615b47abb29517696494c2dae4f0977dd
Simone <startae14@gmail.com>
2022-02-18 18:41:45 +0100

    Retrieve Route params and validate them

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const app = express();
 
 const PORT = 3000;
 
-// define a ROUTE
-app.get('/', (req, res) => {
-  res.send({
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein'
+  },
+  {
     id: 1,
     name: 'Sir Isaac Newton'
-  });
+  }
+];
+
+
+// define a ROUTE
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+// route with param
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  // validate values passed to avoix XSS
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist"
+    });
+  }
 });
 
 app.get('/messages', (req, res) => {
