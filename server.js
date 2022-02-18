3bc1db4daa86d9198d3b37ed802d89a3f8a632ad
Simone <startae14@gmail.com>
2022-02-18 17:37:16 +0100

    Define different routes in Express

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const app = express();
 
 const PORT = 3000;
 
+// define a ROUTE
+app.get('/', (req, res) => {
+  res.send({
+    id: 1,
+    name: 'Sir Isaac Newton'
+  });
+});
+
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>Helloo Albert!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+  console.log('Updating messages...');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
